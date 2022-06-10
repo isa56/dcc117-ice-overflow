950659938cb8f0dbf0d9fc142d3f0585debf4a29
João Pedro <joao.pedreira@estudante.ufjf.br>
2022-06-10 02:26:34 -0300

    Adiciona rotas iniciais da api e controllers de user, post

--- backend-laravel/routes/api.php
+++ backend-laravel/routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\http\Controllers\UsersController;
+use App\Http\Controllers\PostsController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('posts', PostsController::class);
+Route::apiResource('users', UsersController::class);
