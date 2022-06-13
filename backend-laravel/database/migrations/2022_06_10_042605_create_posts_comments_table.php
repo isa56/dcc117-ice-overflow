3e3c132cf06b293e3bdb091e9f057657748df5a9
João Pedro <joao.pedreira@estudante.ufjf.br>
2022-06-13 05:53:58 -0300

    Corrige campo best_answer da tabela de post_comments

--- backend-laravel/database/migrations/2022_06_10_042605_create_posts_comments_table.php
+++ backend-laravel/database/migrations/2022_06_10_042605_create_posts_comments_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('body');
             $table->integer('vote')
                 ->default(0);
-            $table->boolean('best_anwser')
+            $table->boolean('best_answer')
                 ->default(0);
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->foreignId('post_id')->constrained('posts')->onDelete('cascade');
