f5248c69bf4ae9f83fcf7ef2e844280810739dce
Gabriel Frasson <bielfc27@gmail.com>
2022-07-11 16:22:15 -0300

    Home view with basic elements in place

---
+++ frontend-vue/src/components/PrimaryButton.vue
@@ -0,0 +1,25 @@
+<template>
+    <button 
+        class="bg-primary w-full h-[3rem] rounded-lg hover:bg-primary-dark transition-colors"
+        @click="onClick"
+    >
+        {{ content }}
+    </button>
+</template>
+
+<script>
+export default {
+    name: "PrimaryButton",
+    props: {
+        content: String,
+        onClick: Function
+    },
+    data() {
+        return {};
+    }
+}
+</script>
+
+<style scoped>
+    
+</style>
--- frontend-vue/src/views/HomeView.vue
+++ frontend-vue/src/views/HomeView.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="items-center bg-background flex h-full justify-center">
-    <div class="bg-background-dark text-white rounded-lg px-60 py-16">
-      <div class="grid grid-cols-2 divide-x">
-        <div>
-          <h1 class="text-primary text-dx1 mb-8 text-bottom">
-            ICE <span class="text-secondary">Overflow</span>
-          </h1>
+    <div class="bg-background-dark text-white rounded-lg w-[90%]">
+      <div class="flex">
+        <div class="flex-1 flex flex-col items-center justify-between p-4">
+          <div class="flex flex-col items-center">
+            <h1 class="text-primary text-dx1 mb-1 text-bottom text-[3rem]">
+              ICE <span class="text-secondary">Overflow</span>
+            </h1>
+            <p class="max-w-[15rem] text-center">Encontre as respostas para os seus problemas!</p>
+          </div>
+          <div class="flex flex-col items-center">
+            <router-link class="w-[14rem]" to="/login">
+              <primarybutton content="Entrar"/>
+            </router-link>
+            <small class="text-center mt-1">
+              Não possui uma conta? <router-link class="text-primary" to="/criar-conta">Cadastre-se!</router-link>
+            </small>
+          </div>
         </div>
-        <div class="bg-no-repeat bg-right-bottom ..." >
-          <img src="../assets/img/iceImagem.jpg">
+        <div class="bg-no-repeat bg-right-bottom">
+          <img src="../assets/img/iceImagem.jpg" class="object-cover h-full w-auto max-w-[35rem] rounded-r-lg">
         </div>
       </div>
     </div>
@@ -16,8 +27,13 @@
 </template>
 
 <script>
+import PrimaryButton from "@/components/PrimaryButton"
+
 export default {
   name: "HomeView",
+  components: {
+    'primarybutton': PrimaryButton,
+  },
   data() {
     return {
 
