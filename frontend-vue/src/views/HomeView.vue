f446434095de2de94a695948e818cac057df2a9e
dfreita4s <danifp19@gmail.com>
2022-07-10 22:57:38 -0300

    inicio home/aprendendo vue (~socorro isadora)

--- frontend-vue/src/views/HomeView.vue
+++ frontend-vue/src/views/HomeView.vue
@@ -1,11 +1,24 @@
 <template>
-  <div>Home</div>
+  <div class="items-center bg-background flex h-full justify-center">
+    <div class="bg-background-dark text-white rounded-lg px-60 py-16">
+      <div class="grid grid-cols-2 divide-x">
+        <div>
+          <h1 class="text-primary text-dx1 mb-8 text-bottom">
+            ICE <span class="text-secondary">Overflow</span>
+          </h1>
+        </div>
+        <div class="bg-no-repeat bg-right-bottom ..." >
+          <img src="../assets/img/iceImagem.jpg">
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "HomeView",
-  data () {
+  data() {
     return {
 
     }
@@ -14,5 +27,4 @@ export default {
 </script>
 
 <style scoped>
-
 </style>
