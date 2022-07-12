e18e9cf63804e6024f6cdad3835d034416f21173
GabrielBahia <gabriel.bahia@estudante.ufjf.br>
2022-07-12 13:12:09 -0300

    Criando variaveis e usando diretivas nos inputs

--- frontend-vue/src/views/CreatePostView.vue
+++ frontend-vue/src/views/CreatePostView.vue
@@ -8,17 +8,17 @@
                 CRIAR UM NOVO POST
             </h1>
 
-            <form class="w-100" @submit.prevent="validatePost">
+            <form method="POST" class="w-100" @submit.prevent="validateForm">
 
-                <input class="rounded mb-3 px-2 py-3 w-full bg-white text-background-dark" type="text" placeholder="Título" />
-                <textarea class="rounded mb-3 px-2 py-3 w-full bg-white text-background-dark" rows="5" cols="30" type="text" placeholder="Texto" />
+                <input class="rounded mb-3 px-2 py-3 w-full bg-white text-background-dark" type="text" placeholder="Título" v-model="title" />
+                <textarea class="rounded mb-3 px-2 py-3 w-full bg-white text-background-dark" rows="5" cols="30" type="text" placeholder="Texto" v-model="text"/>
                 <br>
 
                 <label class="label-file px-8 py-2 w-full mb-8 text-bold text-background-dark rounded bg-primary text-lg mt-1 px-1.8 py-0.8" for="arquivo">Enviar arquivos</label>
-                <input class="input-file" type="file" name="arquivo" id="arquivo">
+                <input class="input-file" type="file" name="arquivo" id="arquivo" v-model="archive">
 
                 <div class="flex justify-center" >
-                    <input class=" input font-medium send-button px-2 py-2 w-50 h-18 mb-6 text-background-dark rounded bg-primary text-3xl mt-8" type="submit" value="ENVIAR"/>
+                    <input class=" input font-medium send-button px-12 py-2 w-50 h-18 mb-6 text-background-dark rounded bg-primary text-2xl mt-8" type="submit" value="ENVIAR" @click="validateForm"/>
                     <br>
                 </div>
 
@@ -38,8 +38,15 @@ export default {
 
   data () {
     return {
-      
-    }
+      errors : [],
+      title : null,
+      text : null,
+      archive : null
+    },
+    methods: {
+      validateForm() {
+      },
+    },
   }
 
 }
