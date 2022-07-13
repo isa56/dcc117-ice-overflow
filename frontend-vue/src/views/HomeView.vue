64abef4abc019b831d560b593b5abde263a819c0
Gabriel Frasson <bielfc27@gmail.com>
2022-07-13 16:16:12 -0300

    Finish Home page responsivity

--- frontend-vue/src/views/HomeView.vue
+++ frontend-vue/src/views/HomeView.vue
@@ -2,9 +2,9 @@
   <div class="items-center bg-background flex h-full justify-center">
     <div class="bg-background-dark text-white rounded-lg w-[90%]">
       <div class="flex">
-        <div class="flex-1 flex flex-col items-center justify-between p-4">
-          <div class="flex flex-col items-center">
-            <h1 class="text-primary text-dx1 mb-1 text-bottom text-[3rem]">
+        <div class="flex-1 flex flex-col items-center justify-between p-4 min-h-full">
+          <div class="flex flex-col items-center mb-8">
+            <h1 class="text-primary text-dx1 mb-1 text-bottom text-center text-5xl">
               ICE <span class="text-secondary">Overflow</span>
             </h1>
             <p class="max-w-[15rem] text-center">Encontre as respostas para os seus problemas!</p>
@@ -18,8 +18,8 @@
             </small>
           </div>
         </div>
-        <div id= "img-home" class="bg-no-repeat bg-right-bottom">
-          <img src="../assets/img/iceImagem.jpg" class="object-contain h-full w-full hover:object-scale-down">
+        <div id= "img-home" class="bg-no-repeat bg-right-bottom min-h-[28rem]">
+          <img src="../assets/img/iceImagem.jpg" class="object-cover h-full w-full">
         </div>
       </div>
     </div>
