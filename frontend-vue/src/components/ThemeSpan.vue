249538c17118c72734ff934d6ed2fae57f9f41d2
isa56 <isadoraadriana@gmail.com>
2022-07-14 13:06:51 -0300

    Refactor: Arruma span de tema.

--- frontend-vue/src/components/ThemeSpan.vue
+++ frontend-vue/src/components/ThemeSpan.vue
@@ -1,19 +1,12 @@
-  <template>
-
-    <v-chip class="drop-shadow-md" :color="color" label>{{theme}}</v-chip>
-  
-  </template>
+<template>
+  <v-chip class="drop-shadow-md" :color="color" label>{{ theme }}</v-chip>
+</template>
 
 <script>
-
 export default {
   name: "ThemeSpan",
-  props: ['theme', 'color']
-
-}
+  props: ["theme", "color"],
+};
 </script>
 
-<style scoped>
-
-
-</style>
+<style scoped></style>
