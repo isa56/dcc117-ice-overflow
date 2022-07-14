37adbce2e736db844cc1dfcf44eea106272756b0
isa56 <isadoraadriana@gmail.com>
2022-07-14 14:25:15 -0300

    Feat: Adiciona Toast de erros.

--- frontend-vue/src/App.vue
+++ frontend-vue/src/App.vue
@@ -1,24 +1,27 @@
 <template>
   <div id="app">
-    <navbar/>
-    <router-view class="min-w-full min-h-full bg-background text-white"/>
-    <footer-view/>
+    <navbar />
+    <router-view class="min-w-full min-h-full bg-background text-white" />
+    <footer-view />
+    <vtoast ref="vtoast" />
   </div>
 </template>
 
 <script>
-import NavBar from "@/components/Navbar"
-import Footer from "@/components/Footer"
+import NavBar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import ToastMessage from "@/components/ToastMessage";
 
 export default {
   components: {
-    'navbar': NavBar,
-    'footer-view': Footer
-  }
-
-
-}
-
+    navbar: NavBar,
+    "footer-view": Footer,
+    "vtoast": ToastMessage,
+  },
+  mounted() {
+    this.$root.vtoast = this.$refs.vtoast;
+  },
+};
 </script>
 
 <style>
@@ -27,5 +30,4 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
-
 </style>
---
+++ frontend-vue/src/components/ToastMessage.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-snackbar
+    :color="color"
+    :timeout="timeout"
+    v-model="showSnackbar"
+    top
+    right
+    >{{ message }}</v-snackbar
+  >
+</template>
+
+<script>
+export default {
+  name: "ToastMessage",
+  data() {
+    return {
+      showSnackbar: false,
+      message: "",
+      color: "success",
+      timeout: 3000,
+    };
+  },
+  methods: {
+    show(data) {
+      this.message = data.message || "Erro";
+      this.color = data.color || "success";
+      this.timer = data.timer || 3000;
+      this.showSnackbar = true;
+    },
+  },
+};
+</script>
+
+<style></style>
