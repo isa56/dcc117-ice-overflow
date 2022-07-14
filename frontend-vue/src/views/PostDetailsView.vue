4bf79775f54c12067a8408e5b1861c2e0c5891b1
isa56 <isadoraadriana@gmail.com>
2022-07-14 13:06:15 -0300

    Feat: Adiciona tela de visualização de post.

--- frontend-vue/src/views/PostDetailsView.vue
+++ frontend-vue/src/views/PostDetailsView.vue
@@ -19,11 +19,16 @@ export default {
         text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
         upvotes: 16,
         comments: [
-          {author: "ComentadorAnônimo1", text: "What is Lorem Ipsum?"},
-          {author: "ComentadorAnônimo1", text: "Where does it come from?"},
-          {author: "ComentadorAnônimo1", text: "Why do we use it?"},
-          {author: "ComentadorAnônimo1", text: "Where can I get some?"}
-        ]
+          { author: "ComentadorAnônimo1", text: "What is Lorem Ipsum?" },
+          { author: "ComentadorAnônimo1", text: "Where does it come from?" },
+          { author: "ComentadorAnônimo1", text: "Why do we use it?" },
+          { author: "ComentadorAnônimo1", text: "Where can I get some?" },
+        ],
+        themes: [
+          { id: 1, name: "Tema 1", color: "#6BBBB5" },
+          { id: 2, name: "Tema 2", color: "#5F7D87" },
+          { id: 3, name: "Tema 3", color: "white" },
+        ],
       },
     };
   },
