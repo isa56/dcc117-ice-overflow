6756edc859e9df536cdf5ca9c0d12ae749229c61
Gabriel Frasson <bielfc27@gmail.com>
2022-07-21 17:49:01 -0300

    Feat: filter sidebar

--- frontend-vue/src/views/PostsView.vue
+++ frontend-vue/src/views/PostsView.vue
@@ -1,35 +1,43 @@
 <template>
     <div id="containerPost">
         <div class="relative flex min-h-screen">
-            <div class="bg-secondary text-background w-70">
-                <h1 class="text-center text-xl my-10">FILTRO</h1>
+            <aside id="sidebar" class="bg-secondary text-background fixed z-50 rounded-br-md collapsed">
+                <button id="toggle-sidebar" @click="toggleSidebar" class="group absolute top-0 -right-12 bg-secondary text-white h-12 py-1 px-4 rounded-r-md flex items-center hover:text-opacity-80 transition-opacity">
+                    <span id="menu-icon" class="text-3xl">
+                        &#8801;
+                    </span>
+                    <span id="close-icon" class="text-3xl hidden">
+                        &#215;
+                    </span>
+                </button>
+                
+                <h1 class="text-center text-xl my-10 text-white">FILTRO</h1>
                 <div class="mx-5">
                     <form>
                         <input class="rounded my-3 w-full px-3 py-2 bg-white text-background-dark text-center"
                             type="search" placeholder="DCC117" v-model="filter" />
 
-                        <v-select attach="#containerPost" :items="items" label="Tema" solo></v-select>
-                        <div class="grid grid-cols-2 gap-1">
+                        <div id="containerSelect" class="relative">
+                            <v-select attach="#containerSelect" :items="items" label="Tema" solo></v-select>
+                        </div>
+
+                        <div class="grid grid-cols-2 gap-1 mb-24">
                             <input class="input send-button py-2 text-gray text-xs text-center bg-white "
                                 type="submit" value="DESTAQUES" />
                             <input class="input send-button py-2 text-gray text-xs bg-white " type="submit"
                                 value="NOVIDADES" />
-
                         </div>
-                        <br><br><br><br> 
+                         
                         <div>
                             <input
                                 class="input send-button px-3 py-2 w-full mb-6 text-bold text-background-dark rounded bg-primary text-lg mt-1 px-1.8 py-0.8"
                                 type="submit" value="Filtrar" />
-
                         </div>
                     </form>
-
                 </div>
+            </aside>
 
-            </div>
-
-            <main class="container w-[80%] py-8 mx-20 flex flex-col items-center">
+            <main class="container w-[80%] py-8 mx-auto flex flex-col items-center">
                 <div class="w-full flex justify-end">
                     <router-link to="/criar-post">
                         <primary-button class="uppercase w-[15rem]" content="Enviar uma pergunta" />
@@ -45,8 +53,8 @@
 </template>
 
 <script>
-import PrimaryButton from "@/components/PrimaryButton"
-import PostDetailsSummary from "@/components/PostDetailsSummary"
+import PrimaryButton from "@/components/PrimaryButton";
+import PostDetailsSummary from "@/components/PostDetailsSummary";
 
 export default {
     name: "PostsView",
@@ -70,10 +78,25 @@ export default {
         };
     },
     methods: {
-
+        toggleSidebar() {
+            const sidebar = document.querySelector("#sidebar");
+            const menuIcon = document.querySelector("#menu-icon");
+            const closeIcon = document.querySelector("#close-icon");
+            sidebar.classList.toggle("collapsed");
+            menuIcon.classList.toggle("hidden");
+            closeIcon.classList.toggle("hidden");
+        }
     }
 }
 </script>
 
 <style scoped>
+    #sidebar {
+        transition: all 0.2s ease;
+    }
+
+    #sidebar.collapsed {
+        transform: translateX(-100%);
+        transition: all 150ms ease-in 0s;
+    }
 </style>
