605a738a706e85f22e04d570ab5e0ecd8515b5dc
GabrielBahia <gabriel.bahia@estudante.ufjf.br>
2022-07-23 17:47:08 -0300

    Editando template da pagina de perfil

--- frontend-vue/src/views/ProfileView.vue
+++ frontend-vue/src/views/ProfileView.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="flex flex-col px-36 py-8">
 
+    <div class="bg-background-dark mb-4 p-4 rounded-md">
+      <div class="mb-8">
+        <div class="flex mb-2 items-center flex-wrap">
+            <div class="flex space-x-3.5 mb-7">
+                <img class="" src="" alt="fotoPerfil">
+                <h2> {{ name }} </h2>
+                <h3> {{ user }} </h3>
+            </div>
+            <span class="text-base text-white">{{ post.text }}</span>
+        </div>
+      </div>
+    </div>
+
     <PostProfile :post="post" />
 
   </div>
@@ -14,6 +27,9 @@ export default {
   name: "ProfileView",
   data() {
     return {
+      name: 'Joaozinho',
+      user: '@joao123',
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",  
       post: {
         title: "Lorem Ipsum",
         authorName: "GatinhoFeliz123",
