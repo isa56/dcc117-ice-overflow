f8cde7b2a5f919c904f593641d9cb8e637cb2300
Gabriel Frasson <bielfc27@gmail.com>
2022-07-23 16:58:54 -0300

    User Service login integration

--- frontend-vue/src/services/UserService.js
+++ frontend-vue/src/services/UserService.js
@@ -3,52 +3,10 @@ import Api from "@/services/Api";
 
 export default {
   async login(email, password) {
-    console.log(email);
-    console.log(password);
-
-    let formData = new FormData();
-    formData.append("email", email);
-    formData.append("password", password);
-
-    try {
-      const response = await Api()
-        .post("/api/login", formData)
-      return response;
-    } catch (err) {
-      console.log(err.message);
-    }
-
-    // Api()
-    //   .post("/api/login", formData)
-    //   .then(
-    //     (response) => response.data,
-    //     (error) => {
-    //       if (error.message) {
-    //         this.$root.vtoast.show({
-    //           color: "danger",
-    //           message: error.message,
-    //           timeout: 5000,
-    //         });
-    //       }
-
-    //       console.log(error.message);
-
-    //       return Promise.reject(error.message);
-    //     }
-    //   )
-    //   .catch((error) => {
-    //     if (error.message) {
-    //       this.$root.vtoast.show({
-    //         color: "danger",
-    //         message: error.message,
-    //         timeout: 5000,
-    //       });
-    //     }
-
-    //     console.log(error.message);
-
-    //     return Promise.reject(error.message);
-    //   });
+    return await Api().post("/api/login", {
+      email,
+      password
+    });
   },
 
   createAccount({ name, username, email, password }) {
