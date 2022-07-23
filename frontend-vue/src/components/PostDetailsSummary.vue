6b89a0b77c4f1643749d723d25cfc626d33f5fa9
Gabriel Frasson <bielfc27@gmail.com>
2022-07-23 16:51:09 -0300

    Use data from backend on PostDetailsSummary component

--- frontend-vue/src/components/PostDetailsSummary.vue
+++ frontend-vue/src/components/PostDetailsSummary.vue
@@ -2,39 +2,42 @@
   <div class="bg-background-dark w-full my-10 pt-8 p-12 rounded-md">
     <div class="mb-8">
       <div class="flex mb-2 items-baseline justify-start">
-        <h4 class="text-base text-white mr-4">Autor {{ post.authorName }}:</h4>
+        <h4 class="text-base text-white mr-4">Autor ...:</h4>
         <h2 class="text-3xl text-primary font-bold">{{ post.title }}</h2>
       </div>
       <div class="flex">
         <theme-span
           class="mr-2"
-          v-for="theme in post.themes"
-          :key="theme.id"
-          :theme="theme.name"
-          :color="theme.color"
+          :theme="post.materia"
+          color="#6BBBB5"
         />
       </div>
     </div>
 
     <div class="my-8">
-      <p>{{ textResume }}</p>
+      <p>{{ bodySummary }}</p>
     </div>
 
     <div class="flex justify-between text-md">
       <div class="items-center justify-start">
         <div class="mb-4">
           <v-icon color="#F2F7FB">thumb_up</v-icon>
-          {{ post.upvotes }} Reações
+          {{ post.vote }} Reações
         </div>
         <div>
           <v-icon color="#F2F7FB">forum</v-icon>
-          {{ post.commentsCount }} Comentários
+          {{ post.comentarios }} Comentários
         </div>
       </div>
     </div>
 
     <div class="mt-4 text-md">
-      <router-link to="post" class="text-primary">Ver mais >></router-link>
+      <router-link
+        :to="{ name: 'ver-post', params: { id: post.id }}" 
+        class="text-primary"
+      >
+        Ver mais >>
+      </router-link>
     </div>
   </div>
 </template>
@@ -48,7 +51,7 @@ export default {
   components: { ThemeSpan },
   data() {
     return {
-        textResume: this.post.text.length > 500 ? this.post.text.slice(0, 500) + "..." : this.post.text
+        bodySummary: this.post.body.length > 500 ? this.post.body.slice(0, 500) + "..." : this.post.body
     };
   },
 };
