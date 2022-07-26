e07c0eaedf77d19299223f4250c908a500f8a8b9
isa56 <isadoraadriana@gmail.com>
2022-07-26 15:26:18 -0300

    Fix: Corrige API para lidar com os dados recebidos.

--- frontend-vue/src/services/Api.js
+++ frontend-vue/src/services/Api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
-import store from "@/store";
 import { configApi } from "./config";
+import store from "@/store";
+
+import { toastShow } from "../utils/vtoast";
 
 export default () => {
   const api = axios.create(configApi);
@@ -8,77 +10,28 @@ export default () => {
   // https://medium.com/@yaob/how-to-globally-use-axios-instance-and-interceptors-e28f351bb794
   api.interceptors.request.use(
     (config) => {
-      const token = store.get("getAuthToken");
+      const token = store.getters.getAuthToken;
       if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+        config.headers.Authorization = `Bearer ${token}`;
       }
       return config;
     },
     (error) => {
-      // let message;
-      // if (!error.status) {
-        // message = "Não é possível se conectar ao servidor";
-        // this.$root.vtoast.show({
-        //   color: "danger",
-        //   message: message,
-        //   timeout: 5000,
-        // });
-      // }
+      let message;
+      if (!error.status) {
+        message = "Não é possível se conectar ao servidor";
+        toastShow(this.$root.vtoast, message);
+      }
       return Promise.reject(error);
     }
   );
 
   api.interceptors.response.use(
-    (response) => {
-      if (response.status === 200 || response.status === 201) {
-        return Promise.resolve(response);
-      } else {
-        return Promise.reject(response);
-      }
-    },
+    (response) => response,
     (error) => {
-      if (error.response.status) {
-        console.table(error.response.status);
-        switch (error.response.status) {
-          case 400:
-            // this.$root.vtoast.show({
-            //   color: "danger",
-            //   message: "Ocorreu um erro.",
-            //   timeout: 5000,
-            // });
-            break;
-          case 401:
-            // this.$root.vtoast.show({
-            //   color: "danger",
-            //   message: "Você não está logado.",
-            //   timeout: 5000,
-            // });
-            break;
-          case 403:
-            // this.$root.vtoast.show({
-            //   color: "danger",
-            //   message: "Você não possui essa permissão.",
-            //   timeout: 5000,
-            // });
-            break;
-          case 404:
-            // this.$root.vtoast.show({
-            //   color: "danger",
-            //   message: "Esta página não foi encontrada.",
-            //   timeout: 5000,
-            // });
-            break;
-          case 500:
-            // this.$root.vtoast.show({
-            //   color: "danger",
-            //   message: "Existe um erro em nosso servidor.",
-            //   timeout: 5000,
-            // });
-            break;
-        }
+      console.log(error);
 
-        throw error.response;
-      }
+      throw new Error(error);
     }
   );
 
