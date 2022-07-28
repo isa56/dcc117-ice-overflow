be8d3461c37b90885f6dd3cc85942c27e83104a4
Gabriel Frasson <bielfc27@gmail.com>
2022-07-28 16:32:14 -0300

    Fetch all subjects service

---
+++ frontend-vue/src/services/SubjectService.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-prototype-builtins */
+import Api from "@/services/Api";
+
+export default {
+    async fetchAll() {
+        const response = await Api().get("/api/subjects");
+        return response.data;
+    }
+};
